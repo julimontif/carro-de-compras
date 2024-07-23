f21bd7fe4dd03d0c08eea95a6d2e400bcc259620
julimontif <julimontif@gmail.com>
2024-07-23 12:15:36 -0400

    Primera parte del poryecto carrito de compras

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react"
+import Productos from "./components/Productos"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    productos: [
+      { name: "Tomate", price: 1500, img: "/productos/tomate.jpg" },
+      { name: "Arvejas", price: 2500, img: "/productos/arvejas.jpg" },
+      { name: "Lechuga", price: 500, img: "/productos/lechuga.jpg" },
+    ]
+  }
+  render() {
+    return (
+      <div>
+        <Productos 
+        agregarAlCarro={() => console.log("no hace nada")}
+        productos={this.state.productos}
+        />
+      </div>
+    )
+  }
 }
+
 
 export default App;
